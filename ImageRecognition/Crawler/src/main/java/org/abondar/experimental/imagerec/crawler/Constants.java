7c5a9d5296032fb5692abbc11bc24aa283b86012
abondar24 <abondar1992@gmail.com>
2021-06-29 01:12:19 +0200

    crawler handler

--- ImageRecognition/Crawler/src/main/java/org/abondar/experimental/imagerec/crawler/Constants.java
+++ ImageRecognition/Crawler/src/main/java/org/abondar/experimental/imagerec/crawler/Constants.java
@@ -4,5 +4,15 @@ public class Constants {
 
     public static final String DOWNLOAD_ACTION = "download";
 
+    public static final String ANALYZE_ACTION = "analyze";
+
+    public static final String BUCKET_NAME = "img-rec";
+
+    public static final String BUCKET_KEY = "img";
+
+    public static final String ACC_ID = "203212890819";
+
+    public static final String ANL_QUEUE = "ImgRecAnlQueue";
+
     private Constants(){}
 }
--- ImageRecognition/Crawler/src/main/java/org/abondar/experimental/imagerec/crawler/Handler.java
+++ ImageRecognition/Crawler/src/main/java/org/abondar/experimental/imagerec/crawler/Handler.java
@@ -5,7 +5,23 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jsoup.Jsoup;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import static org.abondar.experimental.imagerec.crawler.Constants.ACC_ID;
+import static org.abondar.experimental.imagerec.crawler.Constants.ANALYZE_ACTION;
+import static org.abondar.experimental.imagerec.crawler.Constants.ANL_QUEUE;
+import static org.abondar.experimental.imagerec.crawler.Constants.BUCKET_KEY;
+import static org.abondar.experimental.imagerec.crawler.Constants.BUCKET_NAME;
 import static org.abondar.experimental.imagerec.crawler.Constants.DOWNLOAD_ACTION;
 
 public class Handler implements RequestHandler<SQSEvent, String> {
@@ -26,7 +42,9 @@ public class Handler implements RequestHandler<SQSEvent, String> {
                     var event = mapper.readValue(body, Event.class);
 
                     if (event.getAction().equals(DOWNLOAD_ACTION) && event.getMsg() != null) {
-                        var domain = createUniqueDomain(event.getMsg().getUrl());
+                        var url = new URL(event.getMsg().getUrl());
+                        crawlImage(url, context);
+                        sendToAnalysis(url);
                     }
                 } catch (Exception ex) {
                     logger.log(String.format("Error reading message body: %s", ex.getMessage()));
@@ -36,5 +54,68 @@ public class Handler implements RequestHandler<SQSEvent, String> {
         }
 
         return null;
+    }
+
+
+    private void crawlImage(URL url, Context context) throws IOException {
+        context.getLogger().log(String.format("crawling %s", url.getHost()));
+
+        var doc = Jsoup.connect(url.toString()).post();
+        var elems = doc.getElementsByClass("block");
+
+        for (org.jsoup.nodes.Element blockElem : elems) {
+            var img = blockElem.getElementsByTag("img");
+            var imgUrl = new URL(img.attr("src"));
+            saveImage(new File(imgUrl.getFile()));
+        }
+    }
+
+
+    private void saveImage(File file) {
+        var s3 = S3Client.builder()
+                .region(Region.EU_WEST_1)
+                .build();
+
+        s3.putObject(buildPutRequest(), RequestBody.fromFile(file));
+    }
+
+    private PutObjectRequest buildPutRequest(){
+        return PutObjectRequest.builder()
+                .bucket(BUCKET_NAME)
+                .key(BUCKET_KEY)
+                .build();
+    }
+
+    private void sendToAnalysis(URL url) throws IOException {
+        var queueUrl = String.format("https://sqs.%s.amazonaws.com/%s/%s",
+                Region.EU_WEST_1.toString(), ACC_ID, ANL_QUEUE);
+
+        var msgBody = buildBody(url.getHost());
+
+        var sqs = SqsClient.builder()
+                .region(Region.EU_WEST_1)
+                .build();
+
+        sqs.sendMessage(buildRequest(queueUrl,msgBody));
+
+    }
+
+    private String buildBody(String domain) throws IOException{
+        var event = new Event();
+        var msg = new EventMsg();
+
+        msg.setUrl(domain);
+        event.setAction(ANALYZE_ACTION);
+        event.setMsg(msg);
+
+        return mapper.writeValueAsString(event);
+    }
+
+    private SendMessageRequest buildRequest(String queueUrl,String msgBody){
+       return SendMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .messageBody(msgBody)
+                .delaySeconds(10)
+                .build();
     }
 }
