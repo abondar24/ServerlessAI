0aec391889b8bf981bcbeb211dd14d566897d33d
abondar24 <abondar1992@gmail.com>
2021-06-29 13:20:11 +0200

    test clean

--- ImageRecognition/Crawler/src/test/java/org/abondar/experimental/imagerec/crawler/CrawlerTest.java
+++ ImageRecognition/Crawler/src/test/java/org/abondar/experimental/imagerec/crawler/CrawlerTest.java
@@ -2,14 +2,33 @@ package org.abondar.experimental.imagerec.crawler;
 
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jsoup.Jsoup;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.nullable;
+import static org.mockito.Mockito.when;
 
+@ExtendWith({MockitoExtension.class})
 public class CrawlerTest {
+
 
     @Test
     public void jsonParseTest() throws Exception{
@@ -27,23 +46,4 @@ public class CrawlerTest {
         assertEquals("http://s3-website-eu-west-1.amazonaws.com",event.getMsg().getUrl());
     }
 
-    @Test
-    public void testHandler(){
-        var context = new TestContext();
-        var handler = new Handler();
-
-        var eventBody = "{\n" +
-                "\"action\": \"download\",\n" +
-                "\"msg\": {\n" +
-                "\"url\": \"http://s3-website-eu-west-1.amazonaws.com\"\n" +
-                "}" + "}";
-
-        var event = new SQSEvent();
-        var msg = new SQSEvent.SQSMessage();
-        msg.setBody(eventBody);
-        event.setRecords(List.of(msg));
-
-        var res = handler.handleRequest(event,context);
-        assertTrue(res.contains("OK"));
-    }
-}
+  }
