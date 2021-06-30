3a4d450cbe1478451fe11acfdfa13c2b4056eceb
abondar24 <abondar1992@gmail.com>
2021-06-30 13:15:21 +0200

    tf crawler lambda setup

--- ImageRecognition/cloud-resources/main.tf
+++ ImageRecognition/cloud-resources/main.tf
@@ -24,3 +24,76 @@ resource "aws_sqs_queue" "img_rec_queue" {
 resource "aws_sqs_queue" "img_rec_anl_queue" {
   name                        = "ImgRecAnlQueue"
 }
+
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = var.lambda_exec_role
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+      "sts:AssumeRole"
+      ],
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_log_group" "crawler" {
+  name              = "/aws/lambda/${var.lambda_crawler}"
+  retention_in_days = 14
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+resource "aws_lambda_function" "crawler_func" {
+  filename      = "../Crawler/build/distributions/Crawler-1.0-SNAPSHOT.zip"
+  function_name = var.lambda_crawler
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "Handler.requestHandler"
+
+  source_code_hash = filebase64sha256("../Crawler/build/distributions/Crawler-1.0-SNAPSHOT.zip")
+
+  runtime = "java11"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.crawler,
+  ]
+}
--- ImageRecognition/cloud-resources/variables.tf
+++ ImageRecognition/cloud-resources/variables.tf
@@ -2,3 +2,10 @@ variable region {
   default = "eu-west-1"
 }
 
+variable "lambda_crawler"{
+  default = "crawler"
+}
+
+variable "lambda_exec_role" {
+  default = "lambda_exec_role"
+}
