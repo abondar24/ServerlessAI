ae99f19a724e55b486e6eb997b60305fbfde1e82
abondar24 <abondar1992@gmail.com>
2021-07-16 13:12:16 +0200

    index

---
+++ TodoList/Frontend/src/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'webpack-jquery-ui/css'
+//import {todo} from './todo'
+
+$(function (){
+  //  todo.activate()
+})
