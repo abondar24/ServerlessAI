968f49056c30040f02468090370cf2f54dade60d
abondar24 <abondar1992@gmail.com>
2021-07-18 23:11:50 +0200

    removed dynamoutil

--- TodoList/TodoApi/src/main/java/org/abondar/experimental/todo/api/service/DynamoServiceImpl.java
+++ TodoList/TodoApi/src/main/java/org/abondar/experimental/todo/api/service/DynamoServiceImpl.java
@@ -2,17 +2,21 @@ package org.abondar.experimental.todo.api.service;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.AttributeValueUpdate;
+import com.amazonaws.services.dynamodbv2.model.BatchGetItemRequest;
+import com.amazonaws.services.dynamodbv2.model.BatchGetItemResult;
+import com.amazonaws.services.dynamodbv2.model.DeleteItemRequest;
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
+import com.amazonaws.services.dynamodbv2.model.GetItemResult;
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
-import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
+import com.amazonaws.services.dynamodbv2.model.UpdateItemRequest;
+import org.abondar.experimental.todo.api.data.ItemFields;
 import org.abondar.experimental.todo.api.data.TodoItem;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -20,52 +24,106 @@ import java.util.UUID;
 
 import static org.abondar.experimental.todo.api.constant.Constants.TABLE_NAME;
 
-public class DynamoServiceImpl implements DynamoService{
+public class DynamoServiceImpl implements DynamoService {
 
 
-    private AmazonDynamoDB db;
-    public DynamoServiceImpl(){
-      db = AmazonDynamoDBAsyncClientBuilder.defaultClient();
+    private final AmazonDynamoDB db;
+
+    public DynamoServiceImpl() {
+        db = AmazonDynamoDBAsyncClientBuilder.defaultClient();
     }
 
     @Override
     public void createItem(TodoItem item) throws AmazonServiceException {
-       var id = UUID.randomUUID().toString();
-       item.setId(id);
+        var id = UUID.randomUUID().toString();
+        item.setId(id);
 
-       var dueDate = DynamoUtil.createDate();
-       item.setDueDate(dueDate);
+        var dueDate = createDate();
+        item.setDueDate(dueDate);
 
-       var request = DynamoUtil.buildPutItemRequest(item);
-       db.putItem(request);
+        var request = buildPutItemRequest(item);
+        db.putItem(request);
 
+    }
+
+    private String createDate() {
+        var date = Calendar.getInstance().getTime();
+        var format = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        return format.format(date);
+    }
+
+    private PutItemRequest buildPutItemRequest(TodoItem item) {
+        var request = new PutItemRequest();
+        request.setTableName(TABLE_NAME);
+
+        Map<String, AttributeValue> itemVals = Map.of(
+                ItemFields.id.toString(), new AttributeValue(item.getId()),
+                ItemFields.dueDate.toString(), new AttributeValue(item.getDueDate()),
+                ItemFields.checked.toString(), new AttributeValue(String.valueOf(item.getChecked())),
+                ItemFields.note.toString(), new AttributeValue(item.getNote())
+        );
+
+        request.setItem(itemVals);
+
+        return request;
     }
 
 
     @Override
     public void updateItem(TodoItem item) {
-        var request = DynamoUtil.buildUpdateItemRequest(item);
+        var request = buildUpdateItemRequest(item);
         db.updateItem(request);
+    }
+
+    private UpdateItemRequest buildUpdateItemRequest(TodoItem item){
+        var request  = new UpdateItemRequest();
+        request.setTableName(TABLE_NAME);
+
+        request.addAttributeUpdatesEntry(ItemFields.id.toString(),new AttributeValueUpdate(item.getId()));
+
+        return request;
     }
 
     @Override
     public Optional<TodoItem> readItem(String id) {
-        var request = DynamoUtil.getItemRequest(id);
+        var request = getItemRequest(id);
         var res = db.getItem(request);
-        var item = DynamoUtil.readItemResult(res);
+        var item = readItemResult(res);
         return Optional.of(item);
+    }
+
+    private GetItemRequest getItemRequest(String id) {
+        return null;
+    }
+
+    private TodoItem readItemResult(GetItemResult result) {
+        return null;
     }
 
     @Override
     public List<TodoItem> listItems() {
-        var request = DynamoUtil.getBatchItemRequest();
+        var request = getBatchItemRequest();
         var res = db.batchGetItem(request);
-        return DynamoUtil.readBatchResult(res);
+        return readBatchResult(res);
     }
+
+    private BatchGetItemRequest getBatchItemRequest() {
+        return null;
+    }
+
+    private List<TodoItem> readBatchResult(BatchGetItemResult res) {
+        return null;
+    }
+    
 
     @Override
     public void deleteItem(String id) {
-        var request = DynamoUtil.deleteItemRequest();
+        var request = deleteItemRequest();
         db.deleteItem(request);
     }
+
+    private DeleteItemRequest deleteItemRequest() {
+        return null;
+    }
+
 }
