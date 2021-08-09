5d8406c32350d38922e732d62297308c44a01588
abondar24 <abondar1992@gmail.com>
2021-08-09 21:56:07 +0200

    note api handlers add missing

---
+++ TodoList/NoteApi/src/main/java/org/abondar/experimental/note/api/handler/PollHandler.java
@@ -0,0 +1,57 @@
+package org.abondar.experimental.note.api.handler;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.transcribe.model.GetTranscriptionJobRequest;
+
+import java.io.IOException;
+
+import static org.abondar.experimental.note.api.util.Errors.AWS_NOT_AVAILABLE;
+import static org.abondar.experimental.note.api.util.Errors.JOB_NOT_FOUND;
+import static org.abondar.experimental.note.api.util.Errors.MALFORMED_BODY_ERROR;
+import static org.abondar.experimental.note.api.util.Errors.MSG_FORMAT;
+
+public class PollHandler  extends NoteHandler
+        implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        var logger = context.getLogger();
+
+        var id = getId(input);
+
+        logger.log(String.format("Polling job with id %s",id));
+
+        var res = client.getTranscriptionJob(getJobRequest(id));
+
+            if (res!=null){
+                var job = res.getTranscriptionJob();
+                if (job!=null){
+                    if (job.getTranscriptionJobStatus().equals("COMPLETED")){
+                        logger.log(String.format("Job Status %s",job.getTranscriptionJobStatus()));
+                        return buildResponse(200,job.getTranscriptionJobStatus());
+                    }
+                } else {
+                    logger.log(String.format("Job with id %s not found",id));
+                    return buildResponse(404, String.format(MSG_FORMAT, JOB_NOT_FOUND));
+                }
+            }
+
+        return null;
+    }
+
+
+    private GetTranscriptionJobRequest getJobRequest(String id){
+        var req = new GetTranscriptionJobRequest();
+        req.setTranscriptionJobName(id);
+
+        return req;
+    }
+
+
+
+}
---
+++ TodoList/NoteApi/src/main/java/org/abondar/experimental/note/api/util/Errors.java
@@ -0,0 +1,14 @@
+package org.abondar.experimental.note.api.util;
+
+public class Errors {
+
+    public static final String MSG_FORMAT = "{\"Error\": \"%s\"}";
+    public static final String MALFORMED_BODY_ERROR = "Malformed request or response body";
+
+    public static final String AWS_NOT_AVAILABLE = "AWS infrastructure not available";
+
+    public static final String JOB_NOT_FOUND = "Selected job not found";
+
+
+    private Errors(){}
+}
