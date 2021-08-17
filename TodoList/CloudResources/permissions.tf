f7d7394b89abf3fb1359ed54050186bcd01fda14
abondar24 <abondar1992@gmail.com>
2021-08-18 01:25:24 +0200

    added missing permissions for note lambdas

--- TodoList/CloudResources/permissions.tf
+++ TodoList/CloudResources/permissions.tf
@@ -37,3 +37,19 @@ resource "aws_lambda_permission" "deletePermission" {
   source_arn = "arn:aws:execute-api:${var.region}:${var.acc_id}:${aws_api_gateway_rest_api.todolist.id}/*/${aws_api_gateway_method.deleteMethod.http_method}${aws_api_gateway_resource.todoReadId.path}"
 }
 
+resource "aws_lambda_permission" "noteTranscribePermission" {
+  action = "lambda:InvokeFunction"
+  statement_id = "AllowExecutionFromAPIGateway"
+  principal = "apigateway.amazonaws.com"
+  function_name = aws_lambda_function.transcribe_func.function_name
+  source_arn = "arn:aws:execute-api:${var.region}:${var.acc_id}:${aws_api_gateway_rest_api.note.id}/*/${aws_api_gateway_method.notePostMethod.http_method}${aws_api_gateway_resource.note.path}"
+}
+
+resource "aws_lambda_permission" "notePollPermission" {
+  action = "lambda:InvokeFunction"
+  statement_id = "AllowExecutionFromAPIGateway"
+  principal = "apigateway.amazonaws.com"
+  function_name = aws_lambda_function.poll_func.function_name
+  source_arn = "arn:aws:execute-api:${var.region}:${var.acc_id}:${aws_api_gateway_rest_api.note.id}/*/${aws_api_gateway_method.noteGetMethod.http_method}${aws_api_gateway_resource.notePoll.path}"
+}
+
