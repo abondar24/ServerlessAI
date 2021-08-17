962048c5c61ec89dd85e4c2bfa3a20265e012e77
abondar24 <abondar1992@gmail.com>
2021-08-17 23:47:55 +0200

    new outputd

--- TodoList/CloudResources/outputs.tf
+++ TodoList/CloudResources/outputs.tf
@@ -3,7 +3,18 @@ output "deployment_invoke_url" {
   value       = aws_api_gateway_stage.todoList.invoke_url
 }
 
+output "note_deployment_invoke_url" {
+  value = aws_api_gateway_stage.note.invoke_url
+}
+
+
 output "frontend_bucket_id" {
   description = "Frontend Bucket Id"
   value       = aws_s3_bucket.frb.id
+}
+
+
+output "storage_bucket_id" {
+  description = "Storage Bucket Id"
+  value       = aws_s3_bucket.dtb.id
 }
