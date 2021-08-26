91d20b296694f304a822eb60e349f67982bc9d6f
abondar24 <abondar1992@gmail.com>
2021-08-27 01:34:06 +0200

    day handler

--- TodoList/ScheduleApi/src/main/java/org/abondar/experimental/schedule/api/handler/DayHandler.java
+++ TodoList/ScheduleApi/src/main/java/org/abondar/experimental/schedule/api/handler/DayHandler.java
@@ -7,6 +7,11 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import org.abondar.experimental.schedule.service.DynamoService;
 import org.abondar.experimental.schedule.service.PollyService;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class DayHandler extends ScheduleHandler
         implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
@@ -23,6 +28,23 @@ public class DayHandler extends ScheduleHandler
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        return null;
+        var date = getDate("yyyy-mm-dd hh:mm:ss");
+        var speakDate = getDate("dddd, MMMM Do YYYY");
+
+        var scheduleSpeech = dynamoService.buildSchedule(date,speakDate);
+        var taskResult = pollyService.startTask(scheduleSpeech);
+
+        try {
+            return buildResponse(taskResult);
+        } catch (IOException ex){
+            return  buildErrorResponse(500,ex.getMessage());
+        }
+
+    }
+
+    private String getDate(String format) {
+        var date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(date);
     }
 }
--- TodoList/ScheduleApi/src/main/java/org/abondar/experimental/schedule/api/handler/ScheduleHandler.java
+++ TodoList/ScheduleApi/src/main/java/org/abondar/experimental/schedule/api/handler/ScheduleHandler.java
@@ -2,18 +2,30 @@ package org.abondar.experimental.schedule.api.handler;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.abondar.experimental.schedule.api.data.TaskResult;
 
+import java.io.IOException;
 import java.util.Map;
 
 import static org.abondar.experimental.schedule.util.Constants.HEADERS;
 
 public class ScheduleHandler {
 
-    protected ObjectMapper mapper;
 
-    protected APIGatewayProxyResponseEvent buildResponse(int code, String body) {
+    protected APIGatewayProxyResponseEvent buildResponse(TaskResult body) throws IOException {
         var resp = new APIGatewayProxyResponseEvent();
-        resp.setBody(body);
+        var mapper = new ObjectMapper();
+        resp.setBody(mapper.writeValueAsString(body));
+        resp.setStatusCode(200);
+        resp.setHeaders(HEADERS);
+        resp.setIsBase64Encoded(true);
+
+        return resp;
+    }
+
+    protected APIGatewayProxyResponseEvent buildErrorResponse(int code, String msg)  {
+        var resp = new APIGatewayProxyResponseEvent();
+        resp.setBody(msg);
         resp.setStatusCode(code);
         resp.setHeaders(HEADERS);
         resp.setIsBase64Encoded(true);
