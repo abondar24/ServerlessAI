9db55a10d00ec485ea52fdc4f73a69a571851e2c
abondar24 <abondar1992@gmail.com>
2021-09-02 17:40:01 +0200

    permissions fix

--- IdentityVerifier/CloudResources/permissions.tf
+++ IdentityVerifier/CloudResources/permissions.tf
@@ -11,5 +11,5 @@ resource "aws_lambda_permission" "analysePermission" {
   statement_id = "AllowExecutionFromAPIGateway"
   principal = "apigateway.amazonaws.com"
   function_name = aws_lambda_function.analyse_func.function_name
-  source_arn = "arn:aws:execute-api:${var.region}:${var.acc_id}:${aws_api_gateway_rest_api.anl.id}/*/${aws_api_gateway_method.anlGetMethod.http_method}${aws_api_gateway_resource.anl.path}"
+  source_arn = "arn:aws:execute-api:${var.region}:${var.acc_id}:${aws_api_gateway_rest_api.anl.id}/*/${aws_api_gateway_method.anlGetMethod.http_method}${aws_api_gateway_resource.anlGet.path}"
 }
