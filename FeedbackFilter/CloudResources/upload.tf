7432f918a423b0987487acc0ac76d69e072fa0ff
abondar24 <abondar1992@gmail.com>
2021-09-09 13:37:31 +0200

    etag on upload

--- FeedbackFilter/CloudResources/upload.tf
+++ FeedbackFilter/CloudResources/upload.tf
@@ -3,6 +3,7 @@ resource "aws_s3_bucket_object" "feedback_api_upload" {
   bucket = aws_s3_bucket.lmb.id
   key =  var.lambda_api_zip
   source = "${var.lambda_api_zip_dir}/${var.lambda_api_zip}"
+  etag = filemd5("${var.lambda_api_zip_dir}/${var.lambda_api_zip}")
 }
 
 
@@ -10,4 +11,5 @@ resource "aws_s3_bucket_object" "translate_upload" {
   bucket = aws_s3_bucket.lmb.id
   key =  var.lambda_translate_zip
   source = "${var.lambda_translate_zip_dir}/${var.lambda_translate_zip}"
+  etag = filemd5("${var.lambda_translate_zip_dir}/${var.lambda_translate_zip}")
 }
